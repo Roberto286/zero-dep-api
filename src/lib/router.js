873d14231f40d4a4d839bf13bdd885dd8884ec61
Roberto286 <roberto.saliola96@gmail.com>
2024-10-10 18:21:07 +0200

    feat: registering routes in expressjs style + detecting not supported http methods

--- src/lib/router.js
+++ src/lib/router.js
@@ -1,9 +1,14 @@
+import { HttpMethods } from "../enums/http-methods.enum.js";
+
 export const routes = [];
 
 export const Router = () => {
     return {
         register: (path, method, handler) => {
+            if(!HttpMethods[method]) {
+                throw new Error(`Method ${method} is not supported`)
+            }
             routes.push({ path, method, handler })
         }
     }
-}
+} 
--- src/lib/server.js
+++ src/lib/server.js
@@ -1,10 +1,25 @@
 import { createServer } from "http";
-import { routes } from "./router.js";
+import { Router, routes } from "./router.js";
+import { HttpMethods } from "../enums/http-methods.enum.js";
 
-export const Server = () => createServer((req, res) => {
-    routes.forEach(({ method, path, handler }) => {
-        if(req.method === method && req.url === path) {
-            handler(req, res);
-        }
+const router = Router();
+
+const extendServer = (server) => {
+    server.get = (path, handler) => {
+        router.register(path, HttpMethods.GET, handler)
+    };
+};
+
+export const Server = () => {
+    const server = createServer((req, res) => {
+        routes.forEach(({ method, path, handler }) => {
+            if(req.method === method && req.url === path) {
+                handler(req, res);
+            }
+        });
     });
-});
+
+    extendServer(server);
+
+    return server;
+};
