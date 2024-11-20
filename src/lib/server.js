244ee2b4ca496185ae41b284ee857e676f3b5ce5
Roberto286 <roberto.saliola96@gmail.com>
2024-11-20 14:20:21 +0100

    fix: descructuring bug

--- src/lib/server.js
+++ src/lib/server.js
@@ -6,64 +6,64 @@ import { enhanceResponse } from "./response-enhancer.js";
 import { validateSchema } from "./schema-validator.js";
 
 class CustomServer extends Server {
-    static #instance = null;
+  static #instance = null;
 
-    constructor() {
-        super();
-        this.#registerHttpMethods();
-        this.on('request', this.#handleRequest.bind(this));
-        CustomServer.#instance = this;
+  constructor() {
+    super();
+    this.#registerHttpMethods();
+    this.on("request", this.#handleRequest.bind(this));
+    CustomServer.#instance = this;
+  }
+
+  async #handleRequest(req, res) {
+    try {
+      await this.#applyMiddlewares(req, res);
+      const { handler, options = {} } = Router.findRoute(req) || {};
+      const { schema } = options;
+
+      if (schema && !validateSchema(req.body, schema)) {
+        return this.#sendBadRequest(res);
+      }
+
+      if (handler) {
+        await handler(req, res);
+      } else {
+        this.#sendNotFound(res);
+      }
+    } catch (error) {
+      this.#handleError(error, res);
     }
+  }
 
-    async #handleRequest(req, res) {
-        try {
-            await this.#applyMiddlewares(req, res);
-            const { handler, options } = Router.findRoute(req);
-            const { schema } = options;
+  async #applyMiddlewares(req, res) {
+    enhanceResponse(res);
+    req.body = await parseBody(req);
+  }
 
-            if(schema && !validateSchema(req.body, schema)) {
-                return this.#sendBadRequest(res);
-            }
+  #registerHttpMethods() {
+    Object.values(HttpMethods).forEach((method) => {
+      this[method.toLowerCase()] = (path, handler, schema) => {
+        Router.register(path, method, handler, schema);
+      };
+    });
+  }
 
-            if(handler) {
-                await handler(req, res);
-            }else {
-                this.#sendNotFound(res);
-            }
-        }catch(error) {
-            this.#handleError(error, res);
-        }
-    }
+  #sendNotFound(res) {
+    res.send("Resource not found", 404);
+  }
 
-    async #applyMiddlewares(req, res) {
-        enhanceResponse(res);
-        req.body = await parseBody(req);
-    }
+  #handleError(error, res) {
+    console.error(error);
+    res.send("Internal server error", 500);
+  }
 
-    #registerHttpMethods() {
-        Object.values(HttpMethods).forEach(method => {
-            this[method.toLowerCase()] = (path, handler, schema) => {
-                Router.register(path, method, handler, schema);
-            };
-        });
-    }
+  #sendBadRequest(res) {
+    res.send("Bad request", 400);
+  }
 
-    #sendNotFound(res) {
-        res.send("Resource not found", 404)
-    }
-
-    #handleError(error, res) {
-        console.error(error);
-        res.send("Internal server error", 500)
-    }
-
-    #sendBadRequest(res) {
-        res.send("Bad request", 400)
-    }
-
-    static getInstance() {
-        return CustomServer.#instance || new CustomServer();
-    }
+  static getInstance() {
+    return CustomServer.#instance || new CustomServer();
+  }
 }
 
 export const getServer = CustomServer.getInstance;
