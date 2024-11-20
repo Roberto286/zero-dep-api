f414e489f0be8abbfa5475ad93453e3e73b14aff
Roberto286 <roberto.saliola96@gmail.com>
2024-11-20 23:49:13 +0100

    feat: init sqlite3 database

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
 export const PORT = process.env.PORT || 5687;
+export const DATABASE_PATH = process.env.DATABASE_PATH || "./database.sqlite3";
--- src/api/books/actions.js
+++ src/api/books/actions.js
@@ -1,33 +1,37 @@
 import { Crud } from "../../lib/crud.js";
-
+import { getDatabase } from "../../lib/database.js";
 export class BookActions extends Crud {
+  constructor() {
+    super();
+    this.database = getDatabase();
+  }
 
-    Create() {
-        console.log('Creating Book');
-        return {}
-    }
+  Create() {
+    this.database.exec("show tables");
+    return {};
+  }
 
-    FindAll() {
-        console.log('Finding Books');
-        return []
-    }
+  FindAll() {
+    console.log("Finding Books");
+    return [];
+  }
 
-    FindById(id) {
-        console.log(`Finding Book with id: ${id}`);
-        return {}
-    }
+  FindById(id) {
+    console.log(`Finding Book with id: ${id}`);
+    return {};
+  }
 
-    Update(id) {
-        console.log(`Updating Book with id: ${id}`);
-        return {}
-    }
+  Update(id) {
+    console.log(`Updating Book with id: ${id}`);
+    return {};
+  }
 
-    Patch(id) {
-        console.log(`Updating Book with id: ${id}`);
-        return {}
-    }
+  Patch(id) {
+    console.log(`Updating Book with id: ${id}`);
+    return {};
+  }
 
-    Delete(id) {
-        console.log(`Deleting Book with id: ${id}`);
-    }
+  Delete(id) {
+    console.log(`Deleting Book with id: ${id}`);
+  }
 }
